ff848d1fd20a3aba57b110ab06487c81d1662cbb
Hyundeok Lee <hyundeoklee@Hyundeokui-MacBookPro.local>
2026-01-27 14:34:34 -0800

    YouTube 썸네일 이미지 로딩을 위해 next.config에 img.youtube.com 도메인 추가

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "**.supabase.in",
       },
+      {
+        protocol: "https",
+        hostname: "img.youtube.com",
+      },
     ],
   },
 };
